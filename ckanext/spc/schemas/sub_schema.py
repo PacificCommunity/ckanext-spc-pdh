f4fb0bdff42e0ec5dbdbc3c9ec59395f4a974cf1
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2018-10-17 16:37:17 +0300

    SPCXCFZB-156 / nested validation

--- ckanext/spc/schemas/sub_schema.py
+++ ckanext/spc/schemas/sub_schema.py
@@ -124,6 +124,115 @@ def get_default_agent_schema():
     }
 
 
+def get_default_coverage_schema():
+    return {
+        'geographic_coverage': [
+            get_validator('not_empty'),
+            get_validator('construct_sub_schema')('geographic_coverage'),
+        ],
+        'temporal_coverage': [
+            get_validator('not_empty'),
+            get_validator('construct_sub_schema')('temporal_coverage'),
+        ],
+        'taxonomic_coverage': [
+            get_validator('not_empty'),
+            get_validator('construct_sub_schema')('taxonomic_coverage'),
+        ],
+        '__extras': [get_validator('ignore')],
+        '__junk': [get_validator('ignore')],
+    }
+
+
+def get_default_bounding_coordinates_schema():
+    return {
+        'west_bounding_coordinate': [
+            get_validator('not_empty'),
+            get_validator('spc_float_validator')
+        ],
+        'east_bounding_coordinate': [
+            get_validator('not_empty'),
+            get_validator('spc_float_validator')
+        ],
+        'north_bounding_coordinate': [
+            get_validator('not_empty'),
+            get_validator('spc_float_validator')
+        ],
+        'south_bounding_coordinate': [
+            get_validator('not_empty'),
+            get_validator('spc_float_validator')
+        ],
+        '__extras': [get_validator('ignore')],
+        '__junk': [get_validator('ignore')],
+    }
+
+
+def get_default_geographic_coverage_schema():
+    return {
+        'geographic_description': [get_validator('not_empty'), unicode],
+        'bounding_coordinates': [
+            get_validator('not_empty'),
+            get_validator('construct_sub_schema')('bounding_coordinates')
+        ],
+        '__extras': [get_validator('ignore')],
+        '__junk': [get_validator('ignore')],
+    }
+
+
+def get_default_range_of_dates_schema():
+    return {
+        'begin_date': [
+            get_validator('not_empty'),
+            get_validator('spc_normalize_date'),
+            get_validator('isodate'),
+            get_validator('convert_to_json_if_date'),
+        ],
+        'end_date': [
+            get_validator('not_empty'),
+            get_validator('spc_normalize_date'),
+            get_validator('isodate'),
+            get_validator('convert_to_json_if_date'),
+        ],
+        '__extras': [get_validator('ignore')],
+        '__junk': [get_validator('ignore')],
+    }
+
+
+def get_default_temporal_coverage_schema():
+    return {
+        'range_of_dates': [
+            get_validator('spc_ignore_missing_if_one_of')('single_date_time'),
+            get_validator('construct_sub_schema')('range_of_dates')
+        ],
+        'single_date_time': [
+            get_validator('spc_ignore_missing_if_one_of')('range_of_dates'),
+            get_validator('spc_normalize_date'),
+            get_validator('isodate'),
+            get_validator('convert_to_json_if_date'),
+        ],
+        '__extras': [get_validator('ignore')],
+        '__junk': [get_validator('ignore')],
+    }
+
+
+def get_default_taxonomic_coverage_schema():
+    return {
+        'general_taxonomic_coverage': [get_validator('ignore_empty'), unicode],
+        'taxonomic_classification': get_default_taxonomic_class_schema(),
+        '__extras': [get_validator('ignore')],
+        '__junk': [get_validator('ignore')],
+    }
+
+
+def get_default_taxonomic_class_schema():
+    return {
+        'taxon_rank_name': [get_validator('ignore_empty'), unicode],
+        'taxon_rank_value': [get_validator('not_empty'), unicode],
+        'common_name': [get_validator('ignore_empty'), unicode],
+        '__extras': [get_validator('ignore')],
+        '__junk': [get_validator('ignore')],
+    }
+
+
 def get_default_project_schema():
     return {
         'title': [get_validator('not_empty'), unicode],
