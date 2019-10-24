7df5fbda1a9163e6cd56428678e93f003eadcc36
calexandr <cherniavskyi.alexandr@linkdigital.com.au>
2019-10-24 11:31:40 +0300

    SPCXCFZB-523 / resource with pacificmap view label, hotfix

--- ckanext/spc/helpers.py
+++ ckanext/spc/helpers.py
@@ -14,10 +14,10 @@ from ckan.common import config
 from ckanext.spc.utils import eez
 import ckan.lib.helpers as h
 import ckan.plugins.toolkit as toolkit
+import ckan.logic as logic
 
 logger = logging.getLogger(__name__)
 cache = CacheManager()
-
 
 def get_helpers():
     return dict(
@@ -45,8 +45,17 @@ def countries_list(countries):
         countries_list.append(countries)
     return map(lambda x: x.upper(), countries_list)
 
-def spc_is_valid_cesium_format(fmt):
-    if fmt.lower() in ('wms', 'wfs', 'kml', 'kmz', 'gjson', 'geojson', 'czml'):
+def spc_is_valid_cesium_format(res):
+    cesium = False
+    if res['has_views']:
+        views = toolkit.get_action('resource_view_list')({'user': toolkit.c.user}, {'id': res['id']})
+        cesium = [
+            view['view_type'] 
+            for view in views 
+            if view['view_type'] == 'cesium_view'
+        ]
+    formats = ('kml', 'kmz')
+    if res['format'].lower() in formats and cesium:
         return True
 
 def spc_get_available_languages():
