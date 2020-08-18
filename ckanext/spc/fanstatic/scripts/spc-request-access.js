f695f99f9a90169a1700d6f2362317e4a2465d44
mutantsan <kvaqich@gmail.com>
2020-08-18 15:59:11 +0300

    SPCXCFZB-751 / styles and template

---
+++ ckanext/spc/fanstatic/scripts/spc-request-access.js
@@ -0,0 +1,42 @@
+ckan.module("spc-request-access", function ($) {
+  "use strict";
+  return {
+    initialize: function () {
+      this.el.on("submit", this._onSubmit.bind(this));
+    },
+    teardown: function () {
+      this.el.off("submit");
+    },
+
+    _onSubmit: function (e) {
+      var self = this;
+      e.preventDefault();
+      this._afterRequest.bind(this);
+      this.sandbox.ajax(ckan.url("/dataset/request_for_access"), {
+        method: "POST",
+        data: this.el.serialize(),
+        success: function (resp) {
+          if (resp['message']) {
+            self._afterRequest(resp['message']);
+          }
+        },
+        error: function (err) {
+          console.warn(err);
+          self._afterRequest();
+        },
+      });
+    },
+    _afterRequest: function (message = null) {
+      this.el.closest(".modal").modal("hide");
+      const successModal = $("#request-access-modal-success");
+
+      successModal.modal();
+      if (message) (
+        successModal.find('.modal-title').text(message)
+      )
+      setTimeout(function () {
+        successModal.modal("hide")
+      }, 10000)
+    },
+  };
+});
