c84a5e0ddc1b4bcab7b6f5578825e0be62f0a51d
mutantsan <kvaqich@gmail.com>
2020-08-18 15:56:32 +0300

    SPCXCFZB-751 / write auth functions

--- ckanext/spc/logic/auth/get.py
+++ ckanext/spc/logic/auth/get.py
@@ -1,5 +1,8 @@
-from ckan.authz import is_authorized
+from ckan.authz import is_authorized, is_sysadmin
 import ckan.plugins.toolkit as tk
+from ckan.model import Member
+from ckanext.spc.model import AccessRequest
+from ckan.authz import get_user_id_for_username, is_sysadmin
 
 
 def spc_dcat_show(context, data_dict):
@@ -14,3 +17,41 @@ def spc_thematic_area_list(context, data_dict=None):
 @tk.chained_auth_function
 def datastore_search_sql(up_func, context, data_dict):
     return is_authorized('sysadmin', context)
+
+
+def get_access_request(context, data_dict):
+    return is_authorized('site_read', context, data_dict)
+
+
+def manage_access_requests(context, data_dict):
+    return {'success': _check_admin_or_member(context, data_dict)}
+
+
+def _check_admin_or_member(context, data_dict):
+    session = context['session']
+    user_id = get_user_id_for_username(context['user'])
+    model = context['model']
+
+    is_sys = is_sysadmin(context['user'])
+
+    org_id = data_dict.get('owner_org') or data_dict.get('id')
+    is_member = session.query(model.Member) \
+        .filter_by(group_id=org_id, table_id=user_id) \
+        .filter((model.Member.capacity == 'editor') | (model.Member.capacity == 'admin')) \
+        .first()
+
+    return any((is_sys, is_member))
+
+
+def restrict_dataset_show(context, data_dict):
+    if not context['user']:
+        return {'success': False}
+
+    is_admin_or_member = _check_admin_or_member(context, data_dict)
+    is_accessed = AccessRequest.check_access_to_dataset(
+        context['user'],
+        data_dict['id']
+    )
+
+    authorized = any((is_admin_or_member, is_accessed))
+    return {'success': True} if authorized else {'success': False}
