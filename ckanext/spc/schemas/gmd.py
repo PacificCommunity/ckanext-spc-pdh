931c57c82136c5343b57dafdc7a15e4cd51c3e2b
Yan Rudenko <yan.rudenko@gmail.com>
2020-10-09 16:09:17 +0300

    SXSPCXZFI-45 / Updates

--- ckanext/spc/schemas/gmd.py
+++ ckanext/spc/schemas/gmd.py
@@ -1,4 +1,5 @@
 from ckantoolkit import get_validator
+from ckan.lib.navl.validators import unicode_safe
 
 ne = get_validator('not_empty')
 im = get_validator('ignore_empty')
@@ -10,7 +11,7 @@ def _sub(name):
 
 def get_default_gmd_citation_schema():
     return {
-        'title': [ne, unicode],
+        'title': [ne, unicode_safe],
         'alternate_title': [
             im,
             get_validator('spc_to_json'),
@@ -18,7 +19,7 @@ def get_default_gmd_citation_schema():
             get_validator('list_of_strings')
         ],
         'date': get_default_gmd_date_schema(),
-        'edition': [im, unicode],
+        'edition': [im, unicode_safe],
         'edition_date': [
             im,
             get_validator('spc_normalize_date'),
@@ -28,10 +29,10 @@ def get_default_gmd_citation_schema():
         'cited_responsible_party': get_default_gmd_cited_responsible_party_schema(),
         'presentation_form': get_default_gmd_code_string_schema(),
         'series': [im, _sub('gmd_series')],
-        'other_citation_details': [im, unicode],
-        'collective_title': [im, unicode],
-        'isbn': [im, unicode],
-        'issn': [im, unicode],
+        'other_citation_details': [im, unicode_safe],
+        'collective_title': [im, unicode_safe],
+        'isbn': [im, unicode_safe],
+        'issn': [im, unicode_safe],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
     }
@@ -63,9 +64,9 @@ def get_default_gmd_identification_info_schema():
             get_validator('convert_to_json_if_string'),
             get_validator('list_of_strings')
         ],
-        'environment_description': [im, unicode],
+        'environment_description': [im, unicode_safe],
         'extent': get_default_gmd_extent_schema(),
-        'supplemental_information': [im, unicode],
+        'supplemental_information': [im, unicode_safe],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
     }
@@ -73,7 +74,7 @@ def get_default_gmd_identification_info_schema():
 
 def get_default_gmd_extent_schema():
     return {
-        'description': [im, unicode],
+        'description': [im, unicode_safe],
         'geographic_element': [im, _sub('gmd_geo_element')],
         'vertical_element': [im, _sub('gmd_vertical_element')],
         '__extras': [get_validator('ignore')],
@@ -114,8 +115,8 @@ def get_default_gmd_bounding_box_schema():
 def get_default_gmd_base_identification_schema():
     return {
         'citation': [ne, _sub('gmd_citation')],
-        'abstract': [ne, unicode],
-        'purpose': [im, unicode],
+        'abstract': [ne, unicode_safe],
+        'purpose': [im, unicode_safe],
         'credit': [
             im,
             get_validator('spc_to_json'),
@@ -156,14 +157,14 @@ def get_default_gmd_aggregation_schema():
 
 def get_default_gmd_usage_schema():
     return {
-        'specific_usage': [ne, unicode],
+        'specific_usage': [ne, unicode_safe],
         'usage_date_time': [
             im,
             get_validator('spc_normalize_date'),
             get_validator('isodate'),
             get_validator('convert_to_json_if_date'),
         ],
-        'user_determined_limitations': [im, unicode],
+        'user_determined_limitations': [im, unicode_safe],
         'user_contact_info': _sub('gmd_cited_responsible_party'),
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
@@ -187,11 +188,11 @@ def get_default_gmd_keywords_schema():
 
 def get_default_gmd_format_schema():
     return {
-        'name': [ne, unicode],
-        'version': [ne, unicode],
-        'amendment_number': [im, unicode],
-        'specification': [im, unicode],
-        'file_decompression_technique': [im, unicode],
+        'name': [ne, unicode_safe],
+        'version': [ne, unicode_safe],
+        'amendment_number': [im, unicode_safe],
+        'specification': [im, unicode_safe],
+        'file_decompression_technique': [im, unicode_safe],
         'format_distributor': [im, get_default_gmd_distributor_schema()],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
@@ -210,7 +211,7 @@ def get_default_gmd_distributor_schema():
 
 def get_default_gmd_transfer_schema():
     return {
-        'units_of_distribution': [im, unicode],
+        'units_of_distribution': [im, unicode_safe],
         'transfer_size': [im, get_validator('spc_float_validator')],
         'on_line': get_default_gmd_online_resource_schema(),
         'off_line': [im, _sub('gmd_offline_resource')],
@@ -221,15 +222,15 @@ def get_default_gmd_transfer_schema():
 
 def get_default_gmd_order_process_schema():
     return {
-        'fees': [im, unicode],
+        'fees': [im, unicode_safe],
         'planned_available_date_time': [
             im,
             get_validator('spc_normalize_date'),
             get_validator('isodate'),
             get_validator('convert_to_json_if_date'),
         ],
-        'ordering_instructions': [im, unicode],
-        'turnaround': [im, unicode],
+        'ordering_instructions': [im, unicode_safe],
+        'turnaround': [im, unicode_safe],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
     }
@@ -237,9 +238,9 @@ def get_default_gmd_order_process_schema():
 
 def get_default_gmd_graphic_schema():
     return {
-        'file_name': [ne, unicode],
-        'file_description': [im, unicode],
-        'file_type': [im, unicode],
+        'file_name': [ne, unicode_safe],
+        'file_description': [im, unicode_safe],
+        'file_type': [im, unicode_safe],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
     }
@@ -254,7 +255,7 @@ def get_default_gmd_maintenance_schema():
             get_validator('isodate'),
             get_validator('convert_to_json_if_date'),
         ],
-        'user_defined_maintenance_frequency': [im, unicode],
+        'user_defined_maintenance_frequency': [im, unicode_safe],
         'update_scope': get_default_gmd_code_string_schema(),
         'maintenance_note': [
             im,
@@ -294,7 +295,7 @@ def get_default_gmd_code_string_schema():
 def get_default_gmd_identifier_schema():
     return {
         'authority': [im, _sub('gmd_citation')],
-        'code': [ne, unicode],
+        'code': [ne, unicode_safe],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
     }
@@ -302,9 +303,9 @@ def get_default_gmd_identifier_schema():
 
 def get_default_gmd_cited_responsible_party_schema():
     return {
-        'individual_name': [ne, unicode],
-        'organisation_name': [im, unicode],
-        'position_name': [im, unicode],
+        'individual_name': [ne, unicode_safe],
+        'organisation_name': [im, unicode_safe],
+        'position_name': [im, unicode_safe],
         'contact_info': [im, _sub('gmd_contact')],
         'role': [ne, _sub('gmd_code_string')],
         '__extras': [get_validator('ignore')],
@@ -317,8 +318,8 @@ def get_default_gmd_contact_schema():
         'phone': [im, _sub('gmd_phone')],
         'address': [im, _sub('gmd_address')],
         'online_resource': [im, _sub('gmd_online_resource')],
-        'hours_of_service': [im, unicode],
-        'contact_instructions': [im, unicode],
+        'hours_of_service': [im, unicode_safe],
+        'contact_instructions': [im, unicode_safe],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
     }
@@ -327,10 +328,10 @@ def get_default_gmd_contact_schema():
 def get_default_gmd_online_resource_schema():
     return {
         'linkage': [ne, get_validator('url_validator')],
-        'protocol': [im, unicode],
-        'application_profile': [im, unicode],
-        'name': [im, unicode],
-        'description': [im, unicode],
+        'protocol': [im, unicode_safe],
+        'application_profile': [im, unicode_safe],
+        'name': [im, unicode_safe],
+        'description': [im, unicode_safe],
         'function': [im, _sub('gmd_code_string')],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
@@ -346,10 +347,10 @@ def get_default_gmd_offline_resource_schema():
             get_validator('convert_to_json_if_string'),
             get_validator('list_of_strings')
         ],
-        'density_units': [im, unicode],
+        'density_units': [im, unicode_safe],
         'volumes': [im, get_validator('int_validator')],
         'medium_format': get_default_gmd_code_string_schema(),
-        'medium_note': [im, unicode],
+        'medium_note': [im, unicode_safe],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
     }
@@ -357,9 +358,9 @@ def get_default_gmd_offline_resource_schema():
 
 def get_default_gmd_series_schema():
     return {
-        'name': [im, unicode],
-        'issue_identification': [im, unicode],
-        'page': [im, unicode],
+        'name': [im, unicode_safe],
+        'issue_identification': [im, unicode_safe],
+        'page': [im, unicode_safe],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
     }
@@ -373,10 +374,10 @@ def get_default_gmd_address_schema():
             get_validator('convert_to_json_if_string'),
             get_validator('list_of_strings')
         ],
-        'city': [im, unicode],
-        'administrative_area': [im, unicode],
-        'postal_code': [im, unicode],
-        'country': [im, unicode],
+        'city': [im, unicode_safe],
+        'administrative_area': [im, unicode_safe],
+        'postal_code': [im, unicode_safe],
+        'country': [im, unicode_safe],
         'electronic_mail_address': [
             im,
             get_validator('spc_to_json'),
@@ -480,22 +481,22 @@ def get_default_gmd_metadata_extension_schema():
 
 def get_default_gmd_extended_element_schema():
     return {
-        'name': [ne, unicode],
-        'short_name': [im, unicode],
+        'name': [ne, unicode_safe],
+        'short_name': [im, unicode_safe],
         'domain_code': [im, get_validator('int_validator')],
-        'definition': [ne, unicode],
-        'obligation': [im, unicode],
-        'condition': [im, unicode],
+        'definition': [ne, unicode_safe],
+        'obligation': [im, unicode_safe],
+        'condition': [im, unicode_safe],
         'data_type': [im, _sub('gmd_code_string')],
-        'maximum_occurrence': [im, unicode],
-        'domain_value': [im, unicode],
+        'maximum_occurrence': [im, unicode_safe],
+        'domain_value': [im, unicode_safe],
         'parent_entity': [
             ne,
             get_validator('spc_to_json'),
             get_validator('convert_to_json_if_string'),
             get_validator('list_of_strings')
         ],
-        'rule': [ne, unicode],
+        'rule': [ne, unicode_safe],
         'rationale': [
             im,
             get_validator('spc_to_json'),
@@ -518,7 +519,7 @@ def get_default_gmd_content_info_schema():
 
 def get_default_gmd_coverage_description_schema():
     return {
-        'attribute_description': [ne, unicode],
+        'attribute_description': [ne, unicode_safe],
         'content_type': [ne, _sub('gmd_code_string')],
         'dimension': get_default_gmd_range_dimension_schema(),
         '__extras': [get_validator('ignore')],
@@ -528,8 +529,8 @@ def get_default_gmd_coverage_description_schema():
 
 def get_default_gmd_range_dimension_schema():
     return {
-        'sequence_identifier': [im, unicode],
-        'descriptor': [im, unicode],
+        'sequence_identifier': [im, unicode_safe],
+        'descriptor': [im, unicode_safe],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
     }
@@ -547,7 +548,7 @@ def get_default_gmd_data_quality_schema():
 
 def get_default_gmd_lineage_schema():
     return {
-        'statement': [im, unicode],
+        'statement': [im, unicode_safe],
         'process_step': get_default_gmd_lineage_process_step_schema(),
         'source': get_default_gmd_lineage_source_schema(),
         '__extras': [get_validator('ignore')],
@@ -557,8 +558,8 @@ def get_default_gmd_lineage_schema():
 
 def get_default_gmd_lineage_process_step_schema():
     return {
-        'description': [ne, unicode],
-        'rationale': [im, unicode],
+        'description': [ne, unicode_safe],
+        'rationale': [im, unicode_safe],
         'date_time': [
             im,
             get_validator('spc_normalize_date'),
@@ -579,7 +580,7 @@ def get_default_gmd_lineage_process_step_schema():
 
 def get_default_gmd_lineage_source_schema():
     return {
-        'description': [im, unicode],
+        'description': [im, unicode_safe],
         'scale_denominator': [im, get_validator('int_validator')],
         'source_reference_system': [im, _sub('gmd_identifier')],
         'source_citation': [im, _sub('gmd_citation')],
@@ -598,9 +599,9 @@ def get_default_gmd_dqt_report_schema():
             get_validator('list_of_strings')
         ],
         'measure_identification': [im, _sub('gmd_identifier')],
-        'measure_description': [im, unicode],
+        'measure_description': [im, unicode_safe],
         'evaluation_method_type': [im, _sub('gmd_code_string')],
-        'evaluation_method_description': [im, unicode],
+        'evaluation_method_description': [im, unicode_safe],
         'evaluation_procedure': [im, _sub('gmd_citation')],
         'date_time': [
             im,
@@ -617,9 +618,9 @@ def get_default_gmd_dqt_report_schema():
 def get_default_gmd_dqt_result_schema():
     return {
         'specification': [ne, _sub('gmd_citation')],
-        'explanation': [ne, unicode],
+        'explanation': [ne, unicode_safe],
         'pass': [get_validator('boolean_validator')],
-        'error_statistic': [im, unicode],
+        'error_statistic': [im, unicode_safe],
         'value': [
             ne,
             get_validator('spc_to_json'),
@@ -656,12 +657,12 @@ def get_default_gmd_metadata_constraints_schema():
 def get_default_gmd_application_schema_info_schema():
     return {
         'name': [ne, _sub('gmd_citation')],
-        'schema_language': [ne, unicode],
-        'constraint_language': [ne, unicode],
-        'schema_ascii': [im, unicode],
-        'graphics_file': [im, unicode],
-        'software_development_file': [im, unicode],
-        'software_development_file_format': [im, unicode],
+        'schema_language': [ne, unicode_safe],
+        'constraint_language': [ne, unicode_safe],
+        'schema_ascii': [im, unicode_safe],
+        'graphics_file': [im, unicode_safe],
+        'software_development_file': [im, unicode_safe],
+        'software_development_file_format': [im, unicode_safe],
         '__extras': [get_validator('ignore')],
         '__junk': [get_validator('ignore')],
     }
