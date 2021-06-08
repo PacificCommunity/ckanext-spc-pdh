1ef49c562e46652acbb390502d868ca0ab6c8e5a
Oleksandr Styhar <oleksandr.styhar@linkdigital.com.au>
2021-06-08 16:59:20 +0300

    SPCXCFZB-944 / Shortcut for structured data

--- widgets/search-form/ModeSwitcher.svelte
+++ widgets/search-form/ModeSwitcher.svelte
@@ -24,10 +24,8 @@
         <input id="toggle-enable-solr" type="checkbox" bind:checked={$solrMode} />
         <label for="toggle-enable-solr">Add query syntax to search</label>
         <div class="data-quality-tooltip">
-          <a target="_blank" href="https://solr.apache.org/guide/6_6/common-query-parameters.html">
-            <i class="info-icon"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#001E73"/><rect x="7" y="2" width="2" height="2" fill="white"/><rect x="6" y="6" width="1" height="2" fill="white"/><rect x="6" y="12" width="4" height="2" fill="white"/><rect x="7" y="6" width="2" height="6" fill="white"/></svg></i>
-          </a>
-          <span class="tooltiptext tooltiptext-above data-quality-tooltip-inner info-tooltip">This adds SOLR query language, for more information on how to use click here</span>
+          <i class="info-icon"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#001E73"/><rect x="7" y="2" width="2" height="2" fill="white"/><rect x="6" y="6" width="1" height="2" fill="white"/><rect x="6" y="12" width="4" height="2" fill="white"/><rect x="7" y="6" width="2" height="6" fill="white"/></svg></i>
+          <span class="tooltiptext tooltiptext-above data-quality-tooltip-inner info-tooltip">This adds SOLR query language, for more information on how to use click <a href="https://solr.apache.org/guide/6_6/common-query-parameters.html" target="_blank">here</a></span>
         </div>
     </div>
     <button type=submit class="btn search-btn {$solrMode ? 'hdn' : ''}" on:click={() => dispatch('search')}>
